c4a26c11f1022e3d67b82e55840c7bc2fca25042
Vanderlei Neto <jose.vanderlei@ufba.br>
2024-01-17 00:34:17 -0300

    Ajustes

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -11,5 +11,19 @@ export const dataFake = [
         "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit quo, doloremque rerum hic minus deserunt expedita aliquam, dolorum sunt, velit mollitia. Ipsam, quod! Similique veritatis velit illo assumenda repudiandae fuga.",
         "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5Gvdrf2BNEUqU4VISIOMT3oTkhjJVcQFbHw&usqp=CAU"
 
+    },
+    {
+        "id":"3",
+        "titile":"Saiu nova atualização CODE",
+        "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit quo, doloremque rerum hic minus deserunt expedita aliquam, dolorum sunt, velit mollitia. Ipsam, quod! Similique veritatis velit illo assumenda repudiandae fuga.",
+        "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5Gvdrf2BNEUqU4VISIOMT3oTkhjJVcQFbHw&usqp=CAU"
+
+    },
+    {
+        "id":"4",
+        "titile":"Saiu nova atualização CODE",
+        "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit quo, doloremque rerum hic minus deserunt expedita aliquam, dolorum sunt, velit mollitia. Ipsam, quod! Similique veritatis velit illo assumenda repudiandae fuga.",
+        "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5Gvdrf2BNEUqU4VISIOMT3oTkhjJVcQFbHw&usqp=CAU"
+
     }
 ]
