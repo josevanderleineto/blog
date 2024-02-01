354eb221ed09577dec05209348419594604ddab3
Vanderlei Neto <jose.vanderlei@ufba.br>
2024-01-31 21:15:27 -0300

    Final

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,29 +1,29 @@
 export const dataFake = [
     {
         "id":"1",
-        "titile":"Saiu nova atualização CODE",
-        "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit quo, doloremque rerum hic minus deserunt expedita aliquam, dolorum sunt, velit mollitia. Ipsam, quod! Similique veritatis velit illo assumenda repudiandae fuga.",
-        "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5Gvdrf2BNEUqU4VISIOMT3oTkhjJVcQFbHw&usqp=CAU"
+        "titile":"React",
+        "description": "React é uma biblioteca essencial para qualquer desenvolvedor web que deseja criar aplicações web modernas e eficientes. É uma biblioteca relativamente complexa de aprender, mas é importante entender os princípios básicos para criar aplicações web React. Estes são apenas alguns exemplos de texto que você pode usar para as quatro rotas do seu blog. É importante personalizar o texto para atender ao seu público-alvo e ao seu estilo de escrita. Você também pode adicionar imagens, vídeos e outros recursos para tornar o conteúdo mais envolvente.",
+        "photoCover":"https://thejs.dev/static/4603bf8d46c45eda4c776f516318b6b7/dd13c/hero.jpg"
     },
     {
         "id":"2",
-        "titile":"Saiu nova atualização CODE",
-        "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit quo, doloremque rerum hic minus deserunt expedita aliquam, dolorum sunt, velit mollitia. Ipsam, quod! Similique veritatis velit illo assumenda repudiandae fuga.",
-        "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5Gvdrf2BNEUqU4VISIOMT3oTkhjJVcQFbHw&usqp=CAU"
+        "titile":"Conheça o JavaScript",
+        "description": "JavaScript é uma linguagem de programação que é usada para adicionar interatividade a páginas da web. JavaScript permite criar efeitos, animações e interações com o usuário. JavaScript é uma linguagem de script, o que significa que é executada no lado do cliente. Isso significa que o código JavaScript é executado no navegador do usuário, não no servidor. JavaScript é uma linguagem relativamente complexa de aprender. No entanto, é uma linguagem essencial para qualquer desenvolvedor web que deseja criar páginas da web interativas. JavaScript é uma linguagem essencial para qualquer desenvolvedor web que deseja criar páginas da web interativas. É uma linguagem relativamente complexa de aprender, mas é importante entender os princípios básicos para criar páginas da web interativas.",
+        "photoCover":"https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png"
 
     },
     {
         "id":"3",
-        "titile":"Saiu nova atualização CODE",
-        "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit quo, doloremque rerum hic minus deserunt expedita aliquam, dolorum sunt, velit mollitia. Ipsam, quod! Similique veritatis velit illo assumenda repudiandae fuga.",
-        "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5Gvdrf2BNEUqU4VISIOMT3oTkhjJVcQFbHw&usqp=CAU"
+        "titile":"Saiba mais sobre o CSS",
+        "description": "CSS, ou Cascading Style Sheets, é uma linguagem de folhas de estilo que é usada para estilizar páginas da web. CSS permite controlar a aparência de elementos HTML, como cores, tamanhos, fontes e alinhamento. CSS é uma linguagem de estilo, o que significa que é usado para aplicar estilos a elementos HTML. CSS é uma linguagem de folhas de estilo, o que significa que os estilos são aplicados em cascata. Isso significa que os estilos mais específicos são aplicados primeiro, seguidos pelos estilos mais gerais. CSS é uma linguagem relativamente simples de aprender. No entanto, é importante entender os princípios básicos de CSS para estilizar páginas da web eficazmente. CSS é uma linguagem essencial para qualquer desenvolvedor web. É uma linguagem relativamente simples de aprender, mas é importante entender os princípios básicos para estilizar páginas da web eficazmente.",
+        "photoCover":"https://icons.iconarchive.com/icons/graphics-vibe/developer/256/css-3-icon.png"
 
     },
     {
         "id":"4",
-        "titile":"Saiu nova atualização CODE",
-        "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit quo, doloremque rerum hic minus deserunt expedita aliquam, dolorum sunt, velit mollitia. Ipsam, quod! Similique veritatis velit illo assumenda repudiandae fuga.",
-        "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5Gvdrf2BNEUqU4VISIOMT3oTkhjJVcQFbHw&usqp=CAU"
+        "titile":"Conheçao o HTML",
+        "description": "HTML, ou HyperText Markup Language, é uma linguagem de marcação que define a estrutura e o conteúdo de uma página da web. É a base de todas as páginas da web e é essencial para qualquer desenvolvedor web. HTML é uma linguagem de marcação, o que significa que usa tags para definir o conteúdo da página. As tags são palavras ou símbolos especiais que são usados para indicar o tipo de conteúdo que está sendo exibido. Por exemplo, a tag <h1> é usada para indicar um título de nível 1, a tag <p> é usada para indicar um parágrafo, e assim por diante. HTML também é uma linguagem hierárquica, o que significa que os elementos da página são organizados em uma estrutura de árvore. A estrutura hierárquica é criada usando as tags <div> e <span>. A tag <div> é usada para criar um bloco de conteúdo, enquanto a tag <span> é usada para criar um elemento de conteúdo semântico. HTML é uma linguagem essencial para qualquer desenvolvedor web. É uma linguagem relativamente simples de aprender, mas é importante entender os princípios básicos para criar páginas da web eficazes.",
+        "photoCover":"https://cdn-icons-png.flaticon.com/128/3291/3291670.png"
 
     }
 ]
